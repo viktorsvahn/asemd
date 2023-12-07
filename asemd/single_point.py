794db3f2210cdd9e035991f58df38a3e918b8d07
Viktor Svahn <viksv814@user.uu.se>
2023-12-07 15:03:55 +0100

    reactivated function to remove previous forces

--- asemd/single_point.py
+++ asemd/single_point.py
@@ -27,9 +27,9 @@ class SinglePoint(Configure):
 	def __init__(self, *args):
 		super().__init__(*args)
 
-		if self.mode_params['evaluate'] is not None:
-			for a in self.atoms:
-				a.arrays.pop('forces')
+		#if self.mode_params['evaluate'] is not None:
+		#	for a in self.atoms:
+		#		a.arrays.pop('forces')
 
 
 	def run(self):
@@ -47,24 +47,14 @@ class SinglePoint(Configure):
 			self.evaluate = set(self.mode_params['evaluate'])
 
 			if 'forces' in self.evaluate:
-				for a in self.atoms:
-					a.arrays.pop('forces')
 				self.acquire_property('get_forces')
 			if 'energies' in self.evaluate:
-				for a in self.atoms:
-					a.arrays.pop('energies')
 				self.acquire_property('get_potential_energies')
 			if 'momenta' in self.evaluate:
-				for a in self.atoms:
-					a.arrays.pop('momenta')
 				self.acquire_property('get_momenta')
 			if 'stress' in self.evaluate:
-				for a in self.atoms:
-					a.arrays.pop('stress')
 				self.acquire_property('get_stress')
 			if 'velocities' in self.evaluate:
-				for a in self.atoms:
-					a.arrays.pop('velocities')
 				self.acquire_property('get_velocities')
 			
 		else:
@@ -83,6 +73,7 @@ class SinglePoint(Configure):
 		#results in the correct expression a.get_forces().
 		if self.num_structures > 1:
 			for a in self.atoms:
+				a.arrays.pop(attribute)
 				prop = getattr(a, attribute)()
 		else:
 			prop = getattr(self.atoms, attribute)()
