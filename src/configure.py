69bc5380cb11318ed727b0905eda53c39794527d
viktorsvahn <svahnv@gmail.com>
2023-12-12 12:07:52 +0100

    Moved all source files to /src

---
+++ src/configure.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+
+import copy
+import sys
+import os
+
+from ase.io import read, iread, write
+from ase.io.trajectory import Trajectory
+from ase.calculators.emt import EMT
+from ase import units
+
+
+class Configure(object):
+	"""Setup class that carries shared variables and methods, such as calculator 
+	selection and energy output."""
+	def __init__(self,
+			force_pbc,
+			mode_params,
+			global_params,
+			input_structure,
+			output_structure
+		):
+		self.force_pbc = force_pbc
+		self.mode_params = mode_params
+		self.global_params = global_params
+		self.input_structure = input_structure
+		self.output_structure = output_structure
+
+		# Raise error if no calcualtor has been specified
+		if ('calculator' not in self.mode_params) or (
+				self.mode_params['calculator'] == None):
+			print('Missing calculator!')
+			print('''Choose EMT (for testing) or specify a python script that contains all calculator\ndefinitions by including:\n  calculator: EMT/name_of_script\nin the YAML input file.''')
+		else:
+			self.calculator = self.mode_params['calculator']
+
+		# Collect geometry variables
+		self.pbc = self.global_params['periodic']
+		size = global_params['box size'].split(' ')
+		self.size = [float(s) for s in size]
+		if 'starting index' in self.global_params:
+			self.STARTING_INDEX = self.global_params['starting index']
+		else:
+			self.STARTING_INDEX = -1
+
+		# Input will only be read as a traj if name includes correct extension
+		# Otherwise it will be treated as a .pdb or .xyz file that may, or may
+		# not, include several structures. If this attempt fails, an error is
+		# raised, implying that the input may be a .traj file regardless of its
+		# extension.
+		if 'traj' in self.input_structure:
+			# If file is trajecory, input
+			self.atoms = Trajectory(self.input_structure)[self.STARTING_INDEX]
+			self.atoms.calc = self.acquire_calc(self.calculator)
+			self.atoms.set_cell(self.size)
+			self.atoms.set_pbc(self.pbc)
+		else:
+			# Reads all structures in an atoms object into a list of structures
+			atoms = read(self.input_structure, ':')
+			self.num_structures = len(atoms)
+
+			if self.num_structures > 1:
+				#self.atoms = copy.deepcopy(atoms)
+				self.atoms = atoms
+				for a in self.atoms:
+					a.calc = self.acquire_calc(self.calculator)
+					if self.force_pbc:
+						a.set_cell(self.size)
+					else:
+						pass
+					a.set_pbc(self.pbc)
+			elif self.num_structures == 1:
+				# If list has length 1, atoms object is first, and only, element
+				#self.atoms = copy.deepcopy(atoms[0])
+				self.atoms = atoms[0]
+				self.atoms.calc = self.acquire_calc(self.calculator)
+				if self.force_pbc:
+					self.atoms.set_cell(self.size)
+				else:
+					pass
+				self.atoms.set_pbc(self.pbc)
+			else:
+				raise TypeError('Input structure might be a .traj-file. Change the input extention to .traj and try again.')
+
+
+	def acquire_calc(self, arg=None):
+		"""Method that acquires a chose calculator. If no argument is passed,
+		the method will arbitrarily choose the EMT calculator used for testing 
+		purposes. 
+
+		The calculator used for actual simulations should be defined
+		in a separate python script. To choose such a calculator, this method
+		should be passed with the name of the script as an argument."""
+		if arg == (None or 'EMT'):
+			calculator = EMT()
+		else:
+			calculator = __import__(arg).calculator
+		return calculator
+
+	def print_energy(self):
+		"""Print potential-, kinetic energy (together with temperature) and the 
+		total energy of the system.
+		"""
+		epot = self.atoms.get_potential_energy()/len(self.atoms)
+		ekin = self.atoms.get_kinetic_energy()/len(self.atoms)
+		etot = epot + ekin
+		temp = ekin/(1.5*units.kB)
+		print(f'Energy per atom: Epot: {epot:.4f} eV, Ekin: {ekin:.4f} eV (T: {temp:3.0f} K), Etot: {etot:.4} eV')
---
+++ src/single_point.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import os
+import numpy as np
+
+from ase.io import read, write
+
+from asemd.configure import Configure
+
+
+class SinglePoint(Configure):
+	"""Carries out a single point calculation on all structures in a given 
+	input structure for different properties such as forces and energy.
+
+	Evaluation of charges has not yet been implemented.
+	
+	Methods:
+	run: Runs the single point calculations within the instance object.
+	acquire_property: Evaluates a given property an all atoms-objects found in 
+	the input file. The single point mode has support for input files that 
+	contain multiple structures.
+
+	Supported properties:
+		- Forces
+		- Energies
+		- Momenta
+		- Stress
+		- Velocities"""
+
+
+	
+
+	def __init__(self, *args):
+		super().__init__(*args)
+
+		self.attribute_map = {
+			'forces':'get_forces',
+			'energies':'get_potential_energies',
+			'momenta':'get_momenta',
+			'momenta':'get_momenta',
+			'velocities':'get_velocities'
+		}
+
+	def run(self):
+		"""Runs the single point evaluation of the properties that have been
+		specified in the YAML input file.
+
+		Supported properties:
+		- forces
+		- energies
+		- momenta
+		- stress
+		- velocities"""
+
+		if (self.output_structure is not None) and (
+			os.path.exists(self.output_structure)):
+			try:
+				os.remove(self.output_structure)
+			except:
+				print(f'Could not remove previous output file named: {self.output_structure}')
+				raise
+
+		# Checks to see if properties have been assigned correctly in the input
+		if ('evaluate' in self.mode_params) and (self.mode_params['evaluate'] is not None):
+			self.evaluate = set(self.mode_params['evaluate'])
+
+			# Runs evaluation on all attributes
+			for attribute in self.evaluate:
+				self.acquire_property(attribute)
+			
+		else:
+			print('Nothing to evaluate!')
+			print('Choose a property to evaluate by including:\nevaluate:\n  - property\nin the YAML input file.')
+
+
+	def acquire_property(self, attribute):
+		"""Evaluates the input structure for the properties specified in the 
+		input."""
+		# This is achieved using the getattr-method which concatenates the first
+		# and second arguments as first.second. For example, if first=a and 
+		# second='get_forces', then attr=a.get_forces. The added parenthesis
+		# results in the correct expression a.get_forces().
+		if self.num_structures > 1:
+			for a in self.atoms:
+				a.arrays.pop(attribute)
+				prop = getattr(a, self.attribute_map[attribute])()
+				a.arrays[attribute] = prop
+				self.save_structure(a)
+		else:
+			self.atoms.arrays.pop(attribute)
+			prop = getattr(self.atoms, attribute)()
+			self.atoms.arrays[attribute] = prop
+
+			# It is important to have thius method here, and nu under self.run.
+			# Otherwise ASE will sometimes not forget previous evaluations and
+			# save duplicate evaluations on multiple structres. This requires
+			# append=True to be set. Why this occurs is unclear.
+			self.save_structure(self.atoms)
+
+	def save_structure(self, structure):
+		"""If an output filename has been given, the the output is saved to a
+		file by appending all atoms objects to the file."""
+		if self.output_structure:
+			write(self.output_structure, structure, append=True)
+		else:
+			pass
